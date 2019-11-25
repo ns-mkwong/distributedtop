cda0986174e7298ad0390c9ce06f2e5219921899
Matthew Kwong <mkwong@netskope.com>
2019-11-25 12:32:30 -0800

    fixing Serjan's code

--- agent/agentlib/main.py
+++ agent/agentlib/main.py
@@ -17,11 +17,11 @@ class Agent:
 
   def get_config(self, conf_path):
     result = {}
-    with open(conf_path) af f:
-    try:
-      result = json.loads(f.read())
-    except:
-      print('BAD FORMAT')
+    with open(conf_path) as f:
+      try:
+        result = json.loads(f.read())
+      except:
+        print('BAD FORMAT')
     return result
 
   def shell_command(self, cmd):
@@ -30,25 +30,25 @@ class Agent:
       cmd_run = Popen(cmd, stderr=PIPE, stdout=PIPE, shell=True)
       out, err = cmd_run.communicate()
       if err:
-        print(f'error during execution:\n{err})
+        print(f'error during execution:\n{err}')
         return False
       return True
     except Exception as e:
       print(f'Exception have happened:\n{e}')
 
-  def parse_output(self, cmd_out):
+  def parse_output(self):
     """method to parse output of the shell command."""
     result = []
     with open(self.output_file) as f:
       for index, line in enumerate(f):
         if self.splitter in line:
           break
-      result = f.readlines[index+1:]
+      result = f.readlines()[index+1:]
     return result
 
   def run_top_command(self):
     """actual test#1"""
-    cmd = f"top -b 1 -n 1 -c > {self.agent_output}"
+    cmd = f"top -b 1 -n 1 -c > {self.output_file}"
     cmd_out = self.shell_command(cmd)
     if cmd_out:
       result = self.parse_output()
@@ -60,11 +60,10 @@ class Agent:
     except:
       raise(f'BAD CONFIG:\n{self.configurator}')
     while True:
-      self.runner()
+      self.run_top_command()
       time.sleep(time_cycle)
 
 def main():
-  conf_path = '/etc/agent/agent_config.json'
+  conf_path = '/tmp/agent_config.json'
   a = Agent(conf_path)
   a.runner()
-
